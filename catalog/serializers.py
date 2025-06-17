b846ec98ce21df9d6d7d2fe05c18d9a1898f78a4
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-06-17 16:26:21 +0100

    replaced explicit serializer.Serializer with serializers.ModelSerializer

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from .models import Book
 
-class BookSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    summary= serializers.CharField(max_length=255)
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'summary', 'author']
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # summary= serializers.CharField(max_length=255)

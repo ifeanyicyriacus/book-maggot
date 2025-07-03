f93353e9d79508415e65094a1bed55bc31ca2463
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-07-03 13:06:42 +0100

    Configured media file handling: updated `MEDIA_ROOT` definition, added `static` to serve media files in debug mode.

--- BookMaggot/settings.py
+++ BookMaggot/settings.py
@@ -137,5 +137,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'user.User'
 
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 MEDIA_URL = '/media/'
--- BookMaggot/urls.py
+++ BookMaggot/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.db import router
 from django.urls import path, include
+from django.conf.urls.static import static
 
 import catalog
 
@@ -26,5 +28,8 @@ urlpatterns = [
     # path('user/', include('user.urls')),
     path('', include('catalog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header='Book Maggot Admin'
